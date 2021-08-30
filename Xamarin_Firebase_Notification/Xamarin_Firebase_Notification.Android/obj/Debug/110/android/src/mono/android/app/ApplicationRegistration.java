fb04ca06bea4238199c6b377f6d672a949539f08
Pradeep Kumar <pradeep.kumar@worldfashionexchange.com>
2021-08-30 22:53:57 +0530

    first commit

---
+++ Xamarin_Firebase_Notification/Xamarin_Firebase_Notification.Android/obj/Debug/110/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("Xamarin_Firebase_Notification.Droid.MainApplication, Xamarin_Firebase_Notification.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc64fa27cdadaa286527.MainApplication.class, crc64fa27cdadaa286527.MainApplication.__md_methods);
+		
+	}
+}
---
+++ Xamarin_Firebase_Notification/Xamarin_Firebase_Notification/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Plugin.FirebasePushNotification;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin_Firebase_Notification
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+            CrossFirebasePushNotification.Current.OnTokenRefresh +=
+                Current_OnTokenRefresh;
+        }
+
+        private void Current_OnTokenRefresh(object source,FirebasePushNotificationTokenEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine($"Token: {e.Token}");
+        }
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
